91b249c73feac2ef981777817afa4fdcce634dfd
Eduard-Molchanov <eduard.molchanov@gmail.com>
2021-02-28 17:19:20 +0300

    добавлена функция дебага

---
+++ core/x.php
@@ -0,0 +1,14 @@
+<?php
+
+class x
+{
+    public function __construct()
+    {
+    }
+    public static function x($data)
+    {
+        echo "<pre>";
+        print_r($data);
+        echo "</pre>";
+    }
+}
--- index.php
+++ index.php
@@ -4,6 +4,11 @@
 spl_autoload_register(function ($class) {
 
     include __DIR__ . "/core/{$class}.php";
-
 });
 
+
+$s = DataApi::getData("https://jsonplaceholder.typicode.com/posts");
+
+// print_r($s);
+if($s==Null){echo 42;};
+x::x($s);
